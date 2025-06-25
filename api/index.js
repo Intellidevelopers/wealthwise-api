dcc1fcdd74d72a9275be0ff23780ac0f1190d852
josiah <adeagbojosiah1@gmail.com>
2025-06-25 12:44:33 +0100

    update

--- api/index.js
+++ api/index.js
@@ -1,31 +1,27 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-
-const connectDB = require('../config/db'); // make sure this exports a connectDB() function
+const dotenv = require('dotenv');
+const connectDB = require('../config/db');
 const authRoutes = require('../routes/auth.routes');
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
   res.send('API is live 🚀');
 });
 
-// MongoDB connection flag
 let isConnected = false;
 
-// Vercel-compatible handler
-const handler = async (req, res) => {
+module.exports = async (req, res) => {
   if (!isConnected) {
     await connectDB();
     isConnected = true;
   }
   return serverless(app)(req, res);
 };
-
-module.exports = handler;
