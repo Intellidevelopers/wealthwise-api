7377c99e0ea9fe4c4885ca71f8f2e2be81f543ef
josiah <adeagbojosiah1@gmail.com>
2025-06-29 23:59:48 +0100

    updated

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,9 +1,6 @@
-const axios = require('axios');
-const { v4: uuidv4 } = require('uuid');
-
 exports.initiateFlutterwavePayment = async (req, res) => {
   try {
-    const { amount, email, fullName, courseId } = req.body;
+    const { amount, email, fullName, courseId, customizationTitle } = req.body; // ← added customizationTitle
 
     const tx_ref = `LMS-${uuidv4()}`;
 
@@ -17,7 +14,7 @@ exports.initiateFlutterwavePayment = async (req, res) => {
         name: fullName,
       },
       customizations: {
-        title: 'LMS Course Payment',
+        title: customizationTitle || 'LMS Course Payment', // ← now dynamic
         description: 'Pay to enroll in this course',
       },
       meta: {
